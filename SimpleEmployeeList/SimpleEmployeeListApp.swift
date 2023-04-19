bd6b62a4961aef8f3e87f0eb03a0b36b7e8b58f5
Vojtěch Šimek <v.simek333@gmail.com>
2023-04-19 18:46:46 +0200

    Initial Commit

---
+++ SimpleEmployeeList/SimpleEmployeeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleEmployeeListApp.swift
+//  SimpleEmployeeList
+//
+//  Created by Vojtěch Šimek on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleEmployeeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
